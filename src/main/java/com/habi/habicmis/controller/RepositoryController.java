8ace37f024df9a5805c54dc2589d44aab390ea10
Michael Habermueller <michael.habermueller@sap.com>
2022-12-03 18:23:41 +0100

    Working get, post,  put

--- src/main/java/com/habi/habicmis/controller/RepositoryController.java
+++ src/main/java/com/habi/habicmis/controller/RepositoryController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,7 @@ import com.habi.habicmis.model.Repository;
 import com.habi.habicmis.repository.RepositoryRepository;
 
 @RestController
-@RequestMapping("/repository")
+//@RequestMapping("/repository")
 public class RepositoryController {
 
 	Logger log = LoggerFactory.getLogger(RepositoryController.class); 
@@ -27,12 +30,12 @@ public class RepositoryController {
 		this.repositoryRepository = repRepository;
 	}
 	
-	@PostMapping
+	@PostMapping("/repository")
 	ResponseEntity<Repository> createRepository(@RequestBody Repository repo){
 		log.info("Start Creating a new Repository");
 		log.info(repo.toString());
-		
-		repositoryRepository.findByName(repo.getName()); 
+				
+		 
 		if (repositoryRepository.findByName(repo.getName()) != null) {
 			return new ResponseEntity<>(repo, HttpStatus.ALREADY_REPORTED);
 		}
@@ -42,12 +45,43 @@ public class RepositoryController {
 		return new ResponseEntity<>(repo, HttpStatus.CREATED);		
 	}
 	
-	@GetMapping	
+	@GetMapping("/repository")	
 	ResponseEntity<Iterable<Repository>> getRepositories(){
+		log.info("GetAllRepository Service Requested");
 		return new ResponseEntity<>(repositoryRepository.findAll(), HttpStatus.OK);
 	}
 	
+	@GetMapping("/repository/{name}")
+	ResponseEntity<Repository> getRepositoryByName(@PathVariable String name){
+		log.info("Get Repository By Name Service Requested for repo " + name);
+				
+		Repository repo = repositoryRepository.findByName(name);		
+		if (repo == null) {
+			throw new RepositoryNotFoundException(name);
+		}
+		
+		return new ResponseEntity<>(repo, HttpStatus.OK);				
+		
+	}
 	
+	@PutMapping("/repository/{name}")
+	ResponseEntity<Repository> putRepositorybyName(@RequestBody Repository newRepo,  @PathVariable String name){
+		log.info("Put Repository By Name Service Requested for repo " + name);
+		
+		Repository repo = repositoryRepository.findByName(name);		
+		if (repo == null) {
+			throw new RepositoryNotFoundException(name);
+		}
+		
+		repo.setName(newRepo.getName());
+		//To do: here something more intelligent.
+				
+		repositoryRepository.save(repo);
+		
+		return new ResponseEntity<>(repo, HttpStatus.OK);
+		
+		
+	}
 	
 	
 	
---
+++ src/main/java/com/habi/habicmis/controller/RepositoryNotFoundException.java
@@ -0,0 +1,12 @@
+package com.habi.habicmis.controller;
+
+@SuppressWarnings("serial")
+public class RepositoryNotFoundException extends RuntimeException{
+	
+	public RepositoryNotFoundException(String repo) {
+		
+		super("Repository with name <" + repo + ">not found" );
+	}
+	
+
+}
