0be3487a0e37514fd644fa0f6a9ea8d5ef56495f
Michael Habermueller <michael.habermueller@sap.com>
2022-12-10 12:17:33 +0100

    added Test that inserts state and set it to start state

--- src/test/java/com/habi/habicmis/HabicmisStateMachineTests.java
+++ src/test/java/com/habi/habicmis/HabicmisStateMachineTests.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.habi.habicmis.model.State;
@@ -26,20 +24,33 @@ public class HabicmisStateMachineTests {
 	
 	@Test
 	void createStatemachineTest() {
-		
-			
+				
 		StateMachine sm = new StateMachine();
 		
-		sm.addState(new State("StartState", "This is the Start State of the Machine" ));
+		sm.addState(new State("StartState", "This is the Start State of the Machine" ));		
 		
 		smRepo.save(sm);
 		
 		assertThat(sm.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	void createStatemachineAndSetStartStateTest() {
 		
+		StateMachine sm = new StateMachine();
+		State startState = new State("StartState", "This is the Start State of the machine");
+
+		sm.addState(startState);
 		
+		sm.setStartState(startState);
 		
+		smRepo.save(sm);
+		
+		assertThat(sm.getStartState().getId()).isEqualTo(startState.getId());
 		
 	}
+	
+
 	
 	
 	
